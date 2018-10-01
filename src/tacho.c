ebfd395eb55606b4afcb966bd1c90642d90b090a
Richard Leggett <richard.leggett@earlham.ac.uk>
2018-10-01 16:51:24 +0100

    all sorts

--- src/tacho.c
+++ src/tacho.c
@@ -61,34 +61,37 @@ int main( void )
 			printf( "  port = %s\n", ev3_tacho_port_name( i, s ));
 		}
 	}
-	if ( ev3_search_tacho( LEGO_EV3_M_MOTOR, &sn, 0 )) {
+	if ( ev3_search_tacho( LEGO_EV3_L_MOTOR, &sn, 0 )) {
 		int max_speed;
 
-		printf( "LEGO_EV3_M_MOTOR is found, run for 5 sec...\n" );
+		printf( "LEGO_EV3_L_MOTOR is found, run for 5 sec...\n" );
 		get_tacho_max_speed( sn, &max_speed );
 		printf("  max_speed = %d\n", max_speed );
-		set_tacho_stop_action_inx( sn, TACHO_COAST );
-		set_tacho_speed_sp( sn, max_speed * 2 / 3 );
+		set_tacho_stop_action_inx( sn, TACHO_BRAKE );
+		set_tacho_speed_sp( sn, max_speed /2 );
 		set_tacho_time_sp( sn, 5000 );
-		set_tacho_ramp_up_sp( sn, 2000 );
-		set_tacho_ramp_down_sp( sn, 2000 );
-		set_tacho_command_inx( sn, TACHO_RUN_TIMED );
-		/* Wait tacho stop */
-		Sleep( 100 );
-		do {
-			get_tacho_state_flags( sn, &state );
-		} while ( state );
+		//set_tacho_ramp_up_sp( sn, 2000 );
+		//set_tacho_ramp_down_sp( sn, 2000 );
+		//set_tacho_command_inx( sn, TACHO_RUN_TIMED );
+
+        
+        //run-to-abs-pos
+        /* Wait tacho stop */
+		//Sleep( 100 );
+		//do {
+		//	get_tacho_state_flags( sn, &state );
+		//} while ( state );
 		printf( "run to relative position...\n" );
 		set_tacho_speed_sp( sn, max_speed / 2 );
 		set_tacho_ramp_up_sp( sn, 0 );
 		set_tacho_ramp_down_sp( sn, 0 );
 		set_tacho_position_sp( sn, 90 );
-		for ( i = 0; i < 8; i++ ) {
+		for ( i = 0; i < 2; i++ ) {
 			set_tacho_command_inx( sn, TACHO_RUN_TO_REL_POS );
 			Sleep( 500 );
 		}
 	} else {
-		printf( "LEGO_EV3_M_MOTOR is NOT found\n" );
+		printf( "LEGO_EV3_L_MOTOR is NOT found\n" );
 	}
 	ev3_uninit();
 	printf( "*** ( EV3 ) Bye! ***\n" );
